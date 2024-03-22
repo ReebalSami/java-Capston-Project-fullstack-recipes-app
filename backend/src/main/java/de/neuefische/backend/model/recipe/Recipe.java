ed200ace5baeb2cc86eb656ff1d7e24ad235f619
Reebal <117039208+ReebalSami@users.noreply.github.com>
2024-03-22 10:34:55 +0100

    Update backend/src/main/java/de/neuefische/backend/model/recipe/Recipe.java
    Co-authored-by: ingbeck <156573301+ingbeck@users.noreply.github.com>

--- backend/src/main/java/de/neuefische/backend/model/recipe/Recipe.java
+++ backend/src/main/java/de/neuefische/backend/model/recipe/Recipe.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "recipes")
+@Document("recipes")
 public class Recipe {
     private String id;
     private String name;
