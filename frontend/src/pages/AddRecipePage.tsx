07252cbc4a12de1a968fe5e0e72982c0499e6be9
Reebal Sami <reebal.sami@gmail.com>
2024-03-24 19:32:49 +0100

    Add buttons to add and delete an ingredient. Add selection for type, category and time.

--- frontend/src/pages/AddRecipePage.tsx
+++ frontend/src/pages/AddRecipePage.tsx
@@ -32,10 +32,10 @@ export default function AddRecipePage(props: Readonly<AddRecipePageProps>) {
         }));
     }
     const handleChangeEvent = (
-                                inputType: RecipeFormPrimitiveInputType,
-                                key: string,
-                                event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-                                ) => {
+        inputType: RecipeFormPrimitiveInputType,
+        key: string,
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+    ) => {
 
         let value: RecipeFormPrimitiveInputValue = event.target.value;
 
@@ -49,7 +49,7 @@ export default function AddRecipePage(props: Readonly<AddRecipePageProps>) {
         }
     };
 
-    const handleTimeInput = (key:string, hourInputName: string, minuteInputName: string) => {
+    const handleTimeInput = (key: string, hourInputName: string, minuteInputName: string) => {
         const hours = getFormElementValue(hourInputName);
         const minutes = getFormElementValue(minuteInputName);
         setFormData((prevData) => ({
@@ -74,6 +74,21 @@ export default function AddRecipePage(props: Readonly<AddRecipePageProps>) {
         }));
     };
 
+    const handleAddIngredient = () => {
+        setFormData((prevData) => ({
+            ...prevData,
+            ingredients: [...prevData.ingredients, {name: "", quantity: ""}],
+        }));
+    };
+
+    const handleDeleteIngredient = (index: number) => {
+        const newIngredients = [...formData.ingredients];
+        newIngredients.splice(index, 1);
+        setFormData((prevData) => ({
+            ...prevData,
+            ingredients: newIngredients,
+        }));
+    };
 
     const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -142,53 +157,85 @@ export default function AddRecipePage(props: Readonly<AddRecipePageProps>) {
                     onChange={handleChangeEvent.bind(null, 'string', 'origin')}
                 />
                 <label htmlFor="hours">Preparation Time (hours):</label>
-                <input
-                    type="number"
+                <select
                     id="hours"
                     name="preparationTimeHours"
                     value={formData.preparationTime.hours}
                     onChange={handleTimeInput.bind(null, 'preparationTime', 'preparationTimeHours', 'preparationTimeMinutes')}
-                />
+                >
+                    {[...Array(24).keys()].map((hour) => (
+                        <option key={hour} value={hour}>{hour}</option>
+                    ))}
+                </select>
                 <label htmlFor="minutes">Preparation Time (minutes):</label>
-                <input
-                    type="number"
+                <select
                     id="minutes"
                     name="preparationTimeMinutes"
                     value={formData.preparationTime.minutes}
                     onChange={handleTimeInput.bind(null, 'preparationTime', 'preparationTimeHours', 'preparationTimeMinutes')}
-                />
+                >
+                    {[...Array(60).keys()].map((minute) => (
+                        <option key={minute} value={minute}>{minute}</option>
+                    ))}
+                </select>
                 <label htmlFor="totalTimeHours">Total Time Hours:</label>
-                <input
-                    type="number"
+                <select
                     id="totalTimeHours"
                     name="totalTimeHours"
                     value={formData.totalTime.hours}
                     onChange={handleTimeInput.bind(null, 'totalTime', 'totalTimeHours', 'totalTimeMinutes')}
-                />
+                >
+                    {[...Array(24).keys()].map((hour) => (
+                        <option key={hour} value={hour}>{hour}</option>
+                    ))}
+                </select>
                 <label htmlFor="totalTimeMinutes">Total Time Minutes:</label>
-                <input
-                    type="number"
+                <select
                     id="totalTimeMinutes"
                     name="totalTimeMinutes"
                     value={formData.totalTime.minutes}
                     onChange={handleTimeInput.bind(null, 'totalTime', 'totalTimeHours', 'totalTimeMinutes')}
-                />
+                >
+                    {[...Array(60).keys()].map((minute) => (
+                        <option key={minute} value={minute}>{minute}</option>
+                    ))}
+                </select>
                 <label htmlFor="type">Type:</label>
-                <input
-                    type="text"
+                <select
                     id="type"
                     name="type"
                     value={formData.type}
                     onChange={handleChangeEvent.bind(null, 'array', 'type')}
-                />
+                >
+                    <option value="VEGAN">Vegan</option>
+                    <option value="VEGETARIAN">Vegetarian</option>
+                    <option value="WITH_MEAT">With Meat</option>
+                    <option value="PESCATARIAN">Pescatarian</option>
+                    <option value="GLUTEN_FREE">Gluten Free</option>
+                    <option value="LACTOSE_FREE">Lactose Free</option>
+                    <option value="OTHER">Other</option>
+                </select>
                 <label htmlFor="category">Category:</label>
-                <input
-                    type="text"
+                <select
                     id="category"
                     name="category"
                     value={formData.category}
                     onChange={handleChangeEvent.bind(null, 'array', 'category')}
-                />
+                >
+                    <option value="BREAKFAST">Breakfast</option>
+                    <option value="LUNCH">Lunch</option>
+                    <option value="DINNER">Dinner</option>
+                    <option value="DESSERT">Dessert</option>
+                    <option value="SNACK">Snack</option>
+                    <option value="DRINK">Drink</option>
+                    <option value="APPETIZER">Appetizer</option>
+                    <option value="SALAD">Salad</option>
+                    <option value="SOUP">Soup</option>
+                    <option value="SIDE_DISH">Side Dish</option>
+                    <option value="MAIN_DISH">Main Dish</option>
+                    <option value="BAKING">Baking</option>
+                    <option value="OTHER">Other</option>
+                </select>
                 <label htmlFor="difficulty">Difficulty:</label>
                 <select
                     id="difficulty"
@@ -200,23 +247,30 @@ export default function AddRecipePage(props: Readonly<AddRecipePageProps>) {
                     <option value="MEDIUM">Medium</option>
                     <option value="HARD">Hard</option>
                 </select>
-                <label htmlFor="ingredients">Ingredients:</label>
-                {formData.ingredients.map((ingredient, index) => (
-                    <div key={`${index}-${ingredient.name}`}>
-                        <input
-                            type="text"
-                            name={`ingredient-name-${index}`}
-                            value={ingredient.name}
-                            onChange={(event) => handleChangeIngredients(event, index, 'name')}
-                        />
-                        <input
-                            type="text"
-                            name={`ingredient-quantity-${index}`}
-                            value={ingredient.quantity}
-                            onChange={(event) => handleChangeIngredients(event, index, 'quantity')}
-                        />
-                    </div>
-                ))}
+                <div>
+                    <label htmlFor="ingredients">Ingredients:</label>
+                    {formData.ingredients.map((ingredient, index) => (
+                        <div key={`${index}-${ingredient.name}`}>
+                            <label htmlFor={`ingredient-name-${index}`}>Ingredient Name:</label>
+                            <input
+                                type="text"
+                                name={`ingredient-name-${index}`}
+                                value={ingredient.name}
+                                onChange={(event) => handleChangeIngredients(event, index, 'name')}
+                            />
+                            <label htmlFor={`ingredient-quantity-${index}`}>Quantity:</label>
+                            <input
+                                type="text"
+                                name={`ingredient-quantity-${index}`}
+                                value={ingredient.quantity}
+                                onChange={(event) => handleChangeIngredients(event, index, 'quantity')}
+                            />
+                            <button type="button" onClick={() => handleDeleteIngredient(index)}>Delete</button>
+                        </div>
+                    ))}
+                    <button type="button" onClick={handleAddIngredient}>Add Ingredient</button>
+                </div>
+                <br/>
 
                 <button type="submit">Add Recipe</button>
             </form>
