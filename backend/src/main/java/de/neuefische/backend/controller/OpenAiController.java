35f04afaf051fda1a39dd827b6dd8353cbc1c426
Reebal Sami <reebal.sami@gmail.com>
2024-03-26 16:08:48 +0100

    Add Open AI service in Backend. (Setup, Controller, Service and models.)

---
+++ backend/src/main/java/de/neuefische/backend/controller/OpenAiController.java
@@ -0,0 +1,46 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.model.recipe.RecipeCategory;
+import de.neuefische.backend.model.recipe.RecipeDifficulty;
+import de.neuefische.backend.model.recipe.RecipeType;
+import de.neuefische.backend.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/chat")
+@RequiredArgsConstructor
+public class OpenAiController {
+    private final OpenAiService service;
+
+
+    @GetMapping
+    public String askQuestion(@RequestParam String ingredients) {
+        String types = Arrays.stream(RecipeType.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", ", "'", "'"));
+        String categories = Arrays.stream(RecipeCategory.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", ", "'", "'"));
+        String difficulty = Arrays.stream(RecipeDifficulty.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", ", "'", "'"));
+
+        String q = "give me 3 recipes as json format only with these ingredients: " + ingredients + "The json should include the following attribute: name, description, instructions, author, origin, type, preparationTime, totalTime, category, difficulty and ingredients. " +
+                "categories should include one or more of the following for each Recipe: " + categories + ". " +
+                "types should include one or more of the following for each Recipe: " + types + ". " +
+                "difficulty should include one of the following for each Recipe: " + difficulty + ". " +
+                "PreparationTime and TotalTime should be in the format of {hours: 0, minutes: 0}. " +
+                "Ingredients should include the following attributes: name, amount, unit." +
+                "Description should not be longer than 30 words." +
+                "Instructions should not be longer than 50 words.";
+        return service.askQuestion(q);
+    }
+
+}
---
+++ backend/src/main/java/de/neuefische/backend/model/openai/ChatGptRequest.java
@@ -0,0 +1,32 @@
+package de.neuefische.backend.model.openai;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ChatGptRequest {
+    /**
+     {
+     "model": "gpt-3.5-turbo",
+     "response_format": { "type": "json_object" },
+     "messages": [{"role": "user", "content": "Say this is a test!"}],
+
+     }
+     */
+
+    private String model;
+    private ResponseFormat response_format;
+    private List<ChatGptMessage> messages;
+
+    public ChatGptRequest(String q){
+        this.model = "gpt-3.5-turbo";
+        this.response_format = new ResponseFormat("json_object");
+        this.messages = List.of(new ChatGptMessage(q));
+    }
+}
