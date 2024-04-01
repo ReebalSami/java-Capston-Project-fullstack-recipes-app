da37ac15799d559ecfbcc2db2e60e5ba277f1b21
Reebal Sami <reebal.sami@gmail.com>
2024-04-01 11:40:10 +0200

    Add UserMenu and styling

--- frontend/src/components/userActions/UserAction.tsx
+++ frontend/src/components/userActions/UserAction.tsx
@@ -1,30 +1,109 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import "./UserAction.css";
+
 type UserActionProps = {
     user: string | undefined | null
 
 }
-export default function UserAction(props: Readonly<UserActionProps>) {
+const LoggedOutSettings = ['Login', 'Sign Up'];
+const LoggedInSettings = ['Profile', 'Logout'];
 
-    function login() {
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
+
+export default function UserAction(props: Readonly<UserActionProps>) {
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    function Login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
 
         window.open(host + '/oauth2/authorization/github', '_self')
     }
 
-    function logout() {
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
+    function Logout() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
 
         window.open(host + '/logout', '_self')
     }
 
+    const handleMenuItemClick = (action: string) => {
+        handleCloseUserMenu();
+        if (action === 'Login' || action === 'Sign Up') {
+            Login();
+        } else if (action === 'Logout') {
+            Logout();
+        }
+    };
+
+
     return (
-        <div className={"icon-place login"}>
-            <div>
-                {props.user === null && <button className={"loginButtons"} onClick={login}>Login</button>}
-                {props.user !== null && <button className={"loginButtons"} onClick={logout}>Logout</button>}
-            </div>
-            <div className={"greeting"}>
-                {props.user !== null && <p className={"loginName"}>Hallo {props.user}</p>}
-            </div>
-        </div>
-    );
+        <>
+            <Container maxWidth="xl">
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                    <Box sx={{ flexGrow: 0 }}>
+                        {props.user === null &&
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>}
+                        {props.user !== null &&
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>}
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {props.user === null && LoggedOutSettings.map((setting) => (
+                                <MenuItem key={setting} onClick={() => handleMenuItemClick(setting)}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                            {props.user !== null && LoggedInSettings.map((setting) => (
+                                <MenuItem key={setting} onClick={() => handleMenuItemClick(setting)}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    {props.user !== null &&
+                        <div className={"greeting"}>
+                            <p className={"loginName"}>Hallo {props.user}</p>
+                        </div>
+                    }
+                </Box>
+            </Container>
+        </>
+    )
+        ;
 }
