2cb4c319f29c26bbca2860101f56be11dfa00a82
Reebal Sami <reebal.sami@gmail.com>
2024-04-03 14:37:36 +0200

    Adjust appearance of app's content

--- frontend/src/pages/RecipesPage.tsx
+++ frontend/src/pages/RecipesPage.tsx
@@ -7,19 +7,22 @@ type RecipesPageProps = {
 
 export default function RecipesPage(props: Readonly<RecipesPageProps>) {
     return (
-        <div className="recipe-list">
-            <h2>Recipe List</h2>
-            <div className="recipes-container" style={{display: 'flex', flexWrap: 'wrap'}}>
-                {props.recipes.map((recipe) => (
-                    <RecipeCard key={recipe.id} recipe={recipe}/>
-                ))}
+        <div className="recipes-page-container"> {/* Wrapper div */}
+            <div className="recipe-list">
+                <h2>Recipe List</h2>
+                <div className="recipes-container">
+                    {props.recipes.map((recipe) => (
+                        <RecipeCard key={recipe.id} recipe={recipe}/>
+                    ))}
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </div>
-
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
         </div>
     )
 }
