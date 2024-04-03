970a0d4b4818968bfcf6199f515e8ac9231687cd
Reebal Sami <reebal.sami@gmail.com>
2024-04-03 14:37:16 +0200

    Adjust appearance of app's content

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -3,21 +3,23 @@ import "./Header.css";
 import UserAction from "../userActions/UserAction.tsx";
 import Search from "../search/Search.tsx";
 
-type HeaderProps={
-    user:string | undefined | null
+type HeaderProps = {
+    user: string | undefined | null
 }
 export default function Header(props: Readonly<HeaderProps>) {
 
     return (
         <header>
-            <div className="header-left">
-                <div className="logo"> MyRecipes</div>
-                <Navigation/>
-            </div>
-            <div className="header-right">
-                <Search/>
-                <div className="separator"></div>
-                <UserAction user={props.user}/>
+            <div className="header-content">
+                <div className="header-left">
+                    <div className="logo"> MyRecipes</div>
+                    <Navigation/>
+                </div>
+                <div className="header-right">
+                    <Search/>
+                    <div className="separator"></div>
+                    <UserAction user={props.user}/>
+                </div>
             </div>
         </header>
     )
