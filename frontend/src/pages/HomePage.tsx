c93b85a20f6a1c31b091f44bb1361d4abd62c751
Reebal Sami <reebal.sami@gmail.com>
2024-04-04 11:37:58 +0200

    Adjust Home Page styling and content

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,24 +1,12 @@
-import {useState} from 'react';
 import './HomePage.css';
 import {Recipe} from '../types/Recipe.ts';
-import RecipeCard from "../components/RecipeCard/RecipeCard.tsx";
+import RecipeCarousel from "../components/carousel/RecipeCarousel.tsx";
 
 type HomePageProps = {
     recipes: Recipe[];
 }
 
 export default function HomePage(props: Readonly<HomePageProps>) {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const nextSlide = () => {
-        const newIndex = (currentIndex + 1) % props.recipes.length;
-        setCurrentIndex(newIndex);
-    };
-
-    const prevSlide = () => {
-        const newIndex = (currentIndex - 1 + props.recipes.length) % props.recipes.length;
-        setCurrentIndex(newIndex);
-    };
 
     return (
         <div className="homepage-container">
@@ -26,23 +14,13 @@ export default function HomePage(props: Readonly<HomePageProps>) {
                 src="/images/homepagePhoto.jpeg"
                 alt="Homepage"
                 className="homepage-image"/>
-
             <div className="carousel-section">
-                <h3 className="carousel-title">The Latest Recipes</h3>
-                <div className="carousel-nav">
-                    <button className="prev-button" onClick={prevSlide}>Prev</button>
-                    <div className="carousel">
-                        {props.recipes.map((recipe, index) => (
-                            <div key={recipe.id} className={`carousel-item ${index === currentIndex ? 'active' : ''}`}>
-                                <RecipeCard recipe={recipe}/>
-                                <h4>{recipe.name}</h4>
-                            </div>
-
-                        ))}
-                    </div>
-                    <button className="next-button" onClick={nextSlide}>Next</button>
+                <h3>Latest Recipes</h3>
+                <div className="carousel">
+                    <RecipeCarousel recipes={props.recipes}/>
                 </div>
             </div>
+
             <br/>
             <br/>
             <br/>
