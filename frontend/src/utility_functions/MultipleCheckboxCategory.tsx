868252e6f9fb0ffc9162d3d031864ee3deb4a25e
Reebal Sami <reebal.sami@gmail.com>
2024-04-09 10:40:09 +0200

    fix control component catrgories

--- frontend/src/utility_functions/MultipleCheckboxCategory.tsx
+++ frontend/src/utility_functions/MultipleCheckboxCategory.tsx
@@ -1,7 +1,7 @@
 import {Autocomplete, FormControl} from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
-import {SyntheticEvent} from "react";
+import {SyntheticEvent, useState} from "react";
 import {RecipeCategory} from "../types/Recipe.ts";
 
 
@@ -11,16 +11,19 @@ export type MultipleCheckboxCategoryProps = {
 }
 export default function MultipleCheckboxCategory(props: Readonly<MultipleCheckboxCategoryProps>){
     const optionalCategories=Object.values(RecipeCategory);
+    const [value, setValue] = useState<string[]>([]);
 
     return(
             <FormControl sx={{ m: 1, width: 300 }}>
                 <Stack spacing={3} sx={{ width: 500 }}>
                     <Autocomplete
                         multiple
+                        value={value}
                         id="categories-outlined"
                         options={optionalCategories}
                         defaultValue={props.categories}
                         onChange={(event, value) => {
+                            setValue(value);
                             props.handleCategories(event, value);}}
                         filterSelectedOptions
                         renderInput={(params) => (
